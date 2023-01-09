bda570fad37c2a7e1e1a988ea30bd580e0337937
Catta <catalincatta@yahoo.com>
2023-01-09 11:09:34 +0200

    iproducts

--- src/Codecool.CodecoolShop/Daos/IProductDao.cs
+++ src/Codecool.CodecoolShop/Daos/IProductDao.cs
@@ -1,11 +1,12 @@
 using Codecool.CodecoolShop.Models;
 using System.Collections.Generic;
 
-namespace Codecool.CodecoolShop.Daos
+namespace Codecool.CodecoolShop.Daos;
+
+public interface IProductDao : IDao<Product>
 {
-    public interface IProductDao : IDao<Product>
-    {
-        IEnumerable<Product> GetBy(Supplier supplier);
-        IEnumerable<Product> GetBy(ProductCategory productCategory);
-    }
+    IEnumerable<Product> GetBy(Supplier supplier);
+    IEnumerable<Product> GetBy(ProductCategory productCategory);
+    new IEnumerable<Product> GetAll();
+    public new Product Get(int id);
 }
