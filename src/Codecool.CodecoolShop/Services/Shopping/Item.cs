dfb0337e738bc004de6e0550fe5e0a2c37c643f0
Catta <catalincatta@yahoo.com>
2023-01-09 11:12:10 +0200

    item for cart

---
+++ src/Codecool.CodecoolShop/Services/Shopping/Item.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Services.Shopping;
+
+public class Item
+{
+    public readonly Product Product;
+    public int Number;
+
+    public Item(Product product, int number)
+    {
+        Product = product;
+        Number = number;
+    }
+}
