8ae0d6db23bbe6a6bc6aeec217ecd5a241e402f1
Catta <catalincatta@yahoo.com>
2023-01-10 17:03:37 +0200

    connection string for deserialization

---
+++ src/Codecool.CodecoolShop/Models/ConectionString.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Codecool.CodecoolShop.Models;
+
+public class ConectionString
+{
+    
+    [JsonProperty("DefaultConnection")]
+    public string DefaultConnection { get; set; }
+}
