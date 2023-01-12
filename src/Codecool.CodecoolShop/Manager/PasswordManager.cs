966e2c51ba4943f6174f644b4081e70d95789b37
Catta <catalincatta@yahoo.com>
2023-01-12 10:23:41 +0200

    password hashing and verify

---
+++ src/Codecool.CodecoolShop/Manager/PasswordManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Manager;
+
+public class PasswordManager
+{
+    public static HashSalt GenerateSaltedHash(string password)
+    {
+        var saltBytes = new byte[password.Length];
+        var provider = RandomNumberGenerator.Create();
+        provider.GetNonZeroBytes(saltBytes);
+        var salt = Convert.ToBase64String(saltBytes);
+
+        var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000);
+        var hashPassword = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+
+        var hashSalt = new HashSalt { Hash = hashPassword, Salt = salt };
+        return hashSalt;
+        
+    }
+    public static bool VerifyPassword(string enteredPassword, string storedHash, string storedSalt)
+    {
+        var saltBytes = Convert.FromBase64String(storedSalt);
+        var rfc2898DeriveBytes = new Rfc2898DeriveBytes(enteredPassword, saltBytes, 10000);
+        return Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256)) == storedHash;
+    }
+}
---
+++ src/Codecool.CodecoolShop/Models/HashSalt.cs
@@ -0,0 +1,7 @@
+﻿namespace Codecool.CodecoolShop.Models;
+
+public class HashSalt
+{
+    public string Hash { get; set; }
+    public string Salt { get; set; }
+}
